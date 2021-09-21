e43d1819774f273080ca78cb124e5562ddb812ab
StephGree <89228650+StephGree@users.noreply.github.com>
2021-09-21 11:58:15 -0400

    initial commit

---
+++ app/src/main/java/com/raywenderlich/podplay/repository/ItunesRepo.kt
@@ -0,0 +1,10 @@
+package com.raywenderlich.podplay.repository
+
+import com.raywenderlich.podplay.service.ItunesService
+
+// 1
+class ItunesRepo(private val itunesService: ItunesService) {
+    // 2
+    suspend fun searchByTerm(term: String) =
+        itunesService.searchPodcastByTerm(term) // 3
+}
